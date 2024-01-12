80151e603a866bedb60912955999c53947245403
Ashley Stanton-Nurse <git@analogrelay.net>
2024-01-12 14:57:06 -0800

    higher-half kernel

--- os/oxy-kernel/src/main.rs
+++ os/oxy-kernel/src/main.rs
@@ -1,13 +1,26 @@
 #![no_std]
 #![no_main]
 
+#![feature(error_in_core)]
+#![feature(panic_info_message)]
+
 mod early;
 
+use core::error;
+
 use bootloader_api::{BootloaderConfig, config::Mapping};
-use log::info;
+use log::{info, error, debug};
 
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    if let Some(loc) = info.location() {
+        error!("PANIC! {}({}:{})", loc.file(), loc.line(), loc.column());
+    } else {
+        error!("PANIC! <unknown location>");
+    }
+    if let Some(args) = info.message() {
+        error!("    {}", args);
+    }
     loop {}
 }
 
@@ -23,6 +36,12 @@ fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
 
     info!("Booting Oxygen OS...");
 
-    loop {
+    if log::log_enabled!(log::Level::Debug) {
+        debug!("Kernel Image Physical Address: 0x{:016X}", boot_info.kernel_addr);
+        debug!("Kernel Image Virtual Address: 0x{:016X}", boot_info.kernel_image_offset);
+        debug!("RSDP Address: 0x{:016X}", boot_info.rsdp_addr.as_ref().unwrap());
+        debug!("Kernel Physical Offset: 0x{:016X}", boot_info.physical_memory_offset.as_ref().unwrap());
     }
+
+    panic!("Kernel main reached end of function");
 }
